6b60697f1afc589babfab8d4d0f455c266802f33
Marvin Kurland <bugemarvin@outlook.com>
2024-05-08 00:04:07 +0300

    fuction to get the blogs in total or as a query

--- question-two/api/blog/actions/read_views.py
+++ question-two/api/blog/actions/read_views.py
@@ -8,15 +8,16 @@ from blog.models import Blog
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def BlogListView(request):
-    if request.query_params.get('title'):
-        data = Blog.objects.filter(title=request.query_params.get('title'))
-        if not data:
-            return Response({'error': 'No blog post found with the given title.'}, status=status.HTTP_404_NOT_FOUND)
-    else:
-        data = Blog.objects.all()
+    if request.method == 'GET':
+        if request.query_params.get('title'):
+            data = Blog.objects.filter(title=request.query_params.get('title'))
+            if not data:
+                return Response({'error': 'No blog post found with the given title.'}, status=status.HTTP_404_NOT_FOUND)
+        else:
+            data = Blog.objects.all()
 
-    try:
-        serializer = BlogSerializer(data, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-    except Exception as e:
-        return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        try:
+            serializer = BlogSerializer(data, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
