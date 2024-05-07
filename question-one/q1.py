41ab5b78c692ac744ba25286b377d389380cfe4d
Marvin Kurland <bugemarvin@outlook.com>
2024-05-07 07:38:54 +0300

    initial commit of question 1 in python basics

---
+++ question-one/q1.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+'''
+    Interview Question 1 of python:
+        TODO:
+            1. write a function that takes list of integers as input and returns the sum of all the integers in the list.
+'''
